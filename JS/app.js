56f7c7295b4111ca0df2947819f43921e7fe5b35
MAHasnain <attaulhasnainmaqsoodi@gmail.com>
2025-11-03 13:13:22 +0500

    feat: completed home page structure with dynamic doctor rendering - connected doctors section with Supabase data - added working CTA buttons

--- JS/app.js
+++ JS/app.js
@@ -1,1 +1,19 @@
-console.log(supabase);
+// console.log(supabase);
+
+import { getAllDoctors } from "../Database/allMethods.js";
+
+const allDoctors = document.querySelector(".all-doctors");
+document.addEventListener("DOMContentLoaded", async () => {
+    const doctors = await getAllDoctors();
+    console.log(doctors);
+
+    allDoctors.innerHTML = ``;
+    doctors.map(doctor => {
+        allDoctors.innerHTML += `
+            <div class="member">
+                <img src="${doctor.doctor_img}" width="200px" alt="">
+                <h4>${doctor.doctor_name}</h4>
+                <p>${doctor.specialization}</p>
+             </div>`
+    })
+})
